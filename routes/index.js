2bc07dc753071a5a430c53662703a10ef1e776a1
Ridwan <itsridOfficcial@gmail.com>
2024-05-18 23:04:04 +0530

    final testing

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,5 @@
-var express = require('express');
-var router = express.Router();
-const db = require('../database/db')
-const bodyParser = require('body-parser')
-const personRoutes = require('./personRoutes') //Import the personRoutes.js
-const menuItemRoutes = require('./menuItemRoutes')  //import the menuItemRoutes.js
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
 
-router.use(bodyParser.json());   //it store all data in req.body
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-  
-
-router.use('/person', personRoutes);
-
-router.use('/menu' , menuItemRoutes)
-
-module.exports = router;
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, './src'),
+    path: path.join(__dirname, './routes'),
     filename: 'index.js'
-  }
+  },
+  mode: "none"
 };
