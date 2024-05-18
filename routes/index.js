967a91fce70d6cc15c0eb0fc486830eabee2e8b1
Ridwan <itsridOfficcial@gmail.com>
2024-05-18 23:17:46 +0530

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,20 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
+var express = require('express');
+var router = express.Router();
+const db = require('../database/db')
+const bodyParser = require('body-parser')
+const personRoutes = require('./personRoutes') //Import the personRoutes.js
+const menuItemRoutes = require('./menuItemRoutes')  //import the menuItemRoutes.js
 
-/******/ })()
-;
+router.use(bodyParser.json());   //it store all data in req.body
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+  
+
+router.use('/person', personRoutes);
+
+router.use('/menu' , menuItemRoutes)
+
+module.exports = router;
